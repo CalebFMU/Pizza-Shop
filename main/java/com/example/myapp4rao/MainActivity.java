a238ca475c87776e7ce2c4bbb82dc799c1d77c35
dculp94 <46169494+dculp94@users.noreply.github.com>
2019-04-22 21:03:18 -0400

    Updated activity_order_viewer.xml & MenuItems.java
    Main functionality left that is needed in front end
    -grabbing the values of what is entered from OrderPage 
    -passing that into the Menuitems layout. Then making the + or - buttons remove current or add next item. 
    -Confirm stores information in database and log's the user out on completion.

---
+++ main/java/com/example/myapp4rao/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.myapp4rao;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static DatabaseHelper sqLiteDBHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Manager login
+        sqLiteDBHelper = new DatabaseHelper(this);
+        sqLiteDBHelper.addAttributes("Caleb","Password","Y");
+        sqLiteDBHelper.addAttributes("Harley", "Password","Y");
+        sqLiteDBHelper.addAttributes("David", "Password","Y");
+        sqLiteDBHelper.addAttributes("Kyle", "Password","Y");
+
+        // Employees and public
+        sqLiteDBHelper.addAttributes("John","Doe","N");
+
+        final Button buttonCancel = findViewById(R.id.loginButtonCancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code or methods you want to run
+                final TextView usernameTextFill = findViewById(R.id.usernameTextFill);
+                final TextView passwordTextFill = findViewById(R.id.passwordTextFill);
+                usernameTextFill.setText("");
+                passwordTextFill.setText("");
+            }
+        });
+
+
+
+        final Button buttonOk = findViewById(R.id.loginButtonOk);
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code or methods you want to run
+                final TextView usernameTextFill = findViewById(R.id.usernameTextFill);
+                final TextView passwordTextFill = findViewById(R.id.passwordTextFill);
+                String username = usernameTextFill.getText().toString();
+                String password = passwordTextFill.getText().toString();
+
+                Cursor cursor = sqLiteDBHelper.getUsername(username, password);
+                cursor.moveToFirst();
+                int column = cursor.getColumnIndex(sqLiteDBHelper.getLoginAdmin());
+                String adminStatus = cursor.getString(column);
+                Intent intent;
+                if( cursor.getCount() > 0) {
+                    if( adminStatus.equalsIgnoreCase("Y")) {
+                        intent = new Intent(v.getContext(), NavigationPage.class);
+                    }
+                    else {
+                        intent = new Intent(v.getContext(), OrderPage.class);
+                    }
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                    builder.setMessage("ERROR: Username or Password incorrect").setTitle("Error");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // User clicked OK button
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+
+            }
+        });
+
+}}
---
+++ main/java/com/example/myapp4rao/MenuItems.java
@@ -0,0 +1,22 @@
+package com.example.myapp4rao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_viewer);
+
+        /*****************
+         *
+         * Get to where Menu items is incremented in a for loop for every time user add more items
+         * + and - buttons remove or add items
+         * Get confirm button to prompt a total price and then exit program
+         *
+         *
+         */
+    }
+}
